2b011e87b948ed834200f8e5f5f1f3f46e134983
ubeyid oumer <ubeyidah@gmail.com>
2024-04-05 21:55:11 +0300

    test formatCurrency by jasmine and remove defualt files

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,14 @@
+import formatCurrency from "../scripts/utils/money.js";
+
+describe("test suite: formatCurrency", () => {
+  it("convert cents into dollars", () => {
+    expect(formatCurrency(2095)).toEqual("20.95");
+  });
+  it("working with zero", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+  it("round up to the nearst cent", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+    expect(formatCurrency(2000.1)).toEqual("20.00");
+  });
+});
